185dcfabded5463df7a3ce0179457cc72e63dbaf
София Кирдан <lol_tik77@mail.ru>
2025-01-24 00:45:50 +0500

    выполнена 4 задача 2 урока

---
+++ Task_02_04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace Task_02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+    {
+            int year = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+
+            int delta_day = 24 - day;
+            int delta_month = 1 - month;
+            if (delta_day < 0) delta_month--;
+            int delta_year = 2025 - year;
+            if (delta_month < 0) delta_year--;
+            Console.WriteLine(delta_year >= 18);
+
+
