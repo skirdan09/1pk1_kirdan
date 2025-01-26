91e5eea72609c0247135dc6025e96504bcede5d7
София Кирдан <lol_tik77@mail.ru>
2025-01-26 14:58:27 +0500

    выполнена 8 задача 3 урока

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = 20;
+            int endNumber = 50;
+
+            Console.WriteLine("Числа от 20 до 50, которые делятся на 3, но не делятся на 5:");
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("\nНажмите любую клавишу для выхода.");
+            Console.ReadKey();
+        }
+    }
+}
+    
+
