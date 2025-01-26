96b2a84960180b52e9561bf6698f34a9eddfc743
София Кирдан <lol_tik77@mail.ru>
2025-01-26 10:46:27 +0500

    выполнена 4 задача 3 урока

---
+++ Lesson_03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lesson_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите текст:");
+
+                string userInput = Console.ReadLine();
+
+                if (userInput == "exit" || string.IsNullOrEmpty(userInput))
+                {
+                    break;
+                }
+                count++;
+                Console.Clear();
+            }
+            Console.WriteLine("Количество введенных строк: " + count);
+        }
+
+    }
+}
+    
+
