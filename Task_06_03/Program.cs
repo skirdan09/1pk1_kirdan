d424bdfca5993660b1dc05e50fd66c37219d742c
София Кирдан <lol_tik77@mail.ru>
2025-01-26 14:48:53 +0500

    выполнена 6 задача 3 урока

---
+++ Task_06_03/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_06_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double startX = -4;
+            double endX = 4;
+            double step = 0.5;
+
+            Console.WriteLine("Таблица значений функции y = |x|");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("  x   |   y  ");
+            Console.WriteLine("-----------------------");
+
+            for (double x = startX; x <= endX; x += step)
+            {
+                double y = Math.Abs(x);
+                Console.WriteLine($"{x,5:F1} | {y,5:F1}");
+            }
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("\nНажмите любую клавишу для выхода.");
+            Console.ReadKey();
+        }
+    }
+
+
+}
+    
+
