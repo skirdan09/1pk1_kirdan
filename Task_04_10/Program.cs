c7a5323b84583e5a7445045e10f2f177bf31b827
София Кирдан <lol_tik77@mail.ru>
2025-01-26 18:19:58 +0500

    выполнена 10 задача 4 урока

---
+++ Task_04_10/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task_04_10
+{
+    internal class Program
+    {
+        /*
+        Заполнить массив из 10 элементов случайными числами в интервале [-10..10]
+        и сделать реверс элементов отдельно для 1-ой и 2-ой половин массива.
+        Реверс реализовать через цикл. Стандартные методы класса Array использовать нельзя.
+        */
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-10, 10);
+            }
+            Console.WriteLine("Первоначальные элементы массива: ");
+            foreach (int i in array) Console.Write(i + " ");
+            Console.WriteLine("");
+
+            //Реверс первой части массива
+            for (int i = 0; i < array.Length / 4; i++)
+            {
+                int value = array[i];
+                array[i] = array[array.Length / 2 - 1 - i];
+                array[array.Length / 2 - 1 - i] = value;
+            }
+            //Реверс второй части массива
+            for (int i = array.Length / 2; i < (int)(array.Length * 0.75); i++)
+            {
+                int value = array[i];
+                array[i] = array[(int)(array.Length * 1.5) - 1 - i];
+                array[(int)(array.Length * 1.5) - 1 - i] = value;
+            }
+
+            Console.WriteLine("Элементы массива после преобразований: ");
+            foreach (int i in array) Console.Write(i + " ");
+            Console.WriteLine("");
+        }
+    }
+}
