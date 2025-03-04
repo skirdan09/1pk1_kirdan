44b36fd05f171ff14bc075968d6753fb10cd47d0
София Кирдан <lol_tik77@mail.ru>
2025-03-04 22:43:53 +0500

    выполнена 2 задача 11 урока

---
+++ task_11_01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task_11_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Console.WriteLine($"До вызова метода: x= {x},y={y}");
+            Swap(x, y);
+            Console.WriteLine($"После вызова метода: x= {x},y={y}");
+        }
+        static void Swap(int a,int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
