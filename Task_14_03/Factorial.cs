c311d3f7a61f6182536cea8f16b39f8b0d830e23
skirdan09 <lol_tik77@mail.ru>
2025-05-29 02:19:31 +0500

    выполнена 3 задача 14 урока

---
+++ Task_14_03/Factorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_03
+{
+    internal class Factorial
+    {
+        public static long GetFactorial(int n)
+        {
+            
+            if (n < 0) // условие для неотрцательных чисел
+            {
+                Console.WriteLine("Факториал только для неотрицательных чисел.");
+
+                return 0;
+            }
+
+            long result = 1;
+            for (int i = 2; i <= n; i++) // нахождение факториала
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+    }
+}    
+
---
+++ Task_14_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task_14_03
+{
+    internal class Program
+    {
+        /*Реализуйте статический метод Factorial, 
+         * который принимает целое число и возвращает его факториал. 
+         * Сделайте так, чтобы метод работал только для неотрицательных чисел.
+         */
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите число: ");
+            int number;
+
+                 
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                    long factorial = Factorial.GetFactorial(number);
+                    Console.WriteLine($"Факториал {number} = {factorial}");
+            }
+            
+
+        }
+    }
+}
+
