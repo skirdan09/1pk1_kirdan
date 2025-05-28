dbf97e9d83d8b6c4b9b3bfb90c52c95ff74962a5
skirdan09 <lol_tik77@mail.ru>
2025-05-29 02:29:18 +0500

    выполнена 6 задача 20 урока

---
+++ Task_20_06/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_20_06
+{
+    internal class Program
+    {
+        /*Создайте программу, имитирующую работу светофора, используя перечисление TrafficLightColor:
+        • Red
+        • Yellow
+        • Green
+        Реализуйте автоматическое переключение цветов (каждые 3 секунды). 
+        При смене цвета выводите его в консоль (можно с задержкой Thread.Sleep). 
+        Добавьте возможность ручного переключения (например, по нажатию клавиши).
+         */
+        static void Main(string[] args)
+        {
+            TrafficLight trafficLight = new TrafficLight();
+
+            //  автоматическое переключение в отдельном потоке
+            Thread automaticThread = new Thread(trafficLight.Automatic);
+            automaticThread.Start();
+
+            // ручное переключение
+            trafficLight.Switch();
+        }
+    }
+}
