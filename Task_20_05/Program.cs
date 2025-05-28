da19acbb0471c2dbe74ccdde6eac3c5ca9412c23
skirdan09 <lol_tik77@mail.ru>
2025-05-29 02:28:26 +0500

    выполнена 5 задача 20 урока

---
+++ Task_20_05/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_20_05
+{
+    internal class Program
+    {
+        /*Напишите программу, имитирующую систему авторизации с использованием перечисления AccessLevel:
+        • Guest (только чтение)
+        • User (чтение + комментарии)
+        • Moderator (удаление контента)
+        • Admin (полный доступ)
+        Создайте метод, который проверяет, может ли пользователь выполнить действие (например, удалитьпост).
+        На основе уровня доступа выводите сообщение (например,
+        "Ошибка: Недостаточно прав!").
+         */
+        static void Main(string[] args)
+        {
+            User user1 = new User(AccesLevel.Guest);
+            user1.Reading();
+            user1.DeleteContent();
+
+            User user2 = new User(AccesLevel.User);
+            user2.ReadAndWrite();
+            user2.Full();
+
+            User user3 = new User(AccesLevel.Moderator);
+            user3.ReadAndWrite();
+            user3.DeleteContent();
+            user3.Full();
+
+            User user4 = new User(AccesLevel.Admin);
+            user4.Reading();
+            user4.Full();
+        }
+    }
+}
---
+++ Task_20_05/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_05
+{
+    /*Напишите программу, имитирующую систему авторизации с использованием перечисления AccessLevel:
+        • Guest (только чтение)
+        • User (чтение + комментарии)
+        • Moderator (удаление контента)
+        • Admin (полный доступ)
+     * Создайте метод, который проверяет, может ли пользователь выполнить действие (например, удалить пост).
+        На основе уровня доступа выводите сообщение (например,
+        "Ошибка: Недостаточно прав!").
+     */
+    internal class User
+    {
+        AccesLevel user;
+
+        public User(AccesLevel user)
+        {
+            this.user = user;
+        }
+
+        public void Reading()
+        {
+            if (user < AccesLevel.User)
+            Console.WriteLine($"{user} имеет доступ только к чтению");
+            
+            else Console.WriteLine("Ошибка: Недостаточно прав!");
+            
+        }
+
+        public void ReadAndWrite()
+        {
+            if (user >= AccesLevel.User)
+                Console.WriteLine($"{user} может читать и писать");
+            else Console.WriteLine("Ошибка: Недостаточно прав, чтобы писать!");
+        }
+
+        public void DeleteContent()
+        {
+            if (user >= AccesLevel.Moderator)
+                Console.WriteLine($"{user} может удалять контент");
+            else Console.WriteLine("Ошибка: Недостаточно прав, чтобы удалять контент");
+        }
+
+        public void Full()
+        {
+            if (user > AccesLevel.Moderator)
+                Console.WriteLine($"{user} может все");
+            else Console.WriteLine("Ошибка: недостаточно прав, чтобы иметь полный доступ");
+        }
+    }
+}
